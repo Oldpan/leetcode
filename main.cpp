d5c29b579f856a1d57308c2af7d8316ac2fe4f13
oldpan <295484914@qq.com>
2019-08-11 17:12:00 +0800

    add some leetcode

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <sstream>
 #include <vector>
 #include <unordered_map>
+#include <ctype.h>
+#include <stack>
 
 #include "test/test_concepts.h"
 //#include "test/test_thread.h"
@@ -15,12 +17,132 @@
 using namespace std;
 
 
+//vector<int> GetLeastNumbers_Solution(vector<int> input, int k) {
+//
+//    priority_queue<int, vector<int>> min_heap;
+//    vector<int> result;
+//    for(int i = 0; i < input.size(); i ++)
+//    {
+//        if(min_heap.size() < k)
+//        {
+//            min_heap.push(input[i]);
+//            continue;
+//        }
+//        if(min_heap.top() > input[i])
+//        {
+//            min_heap.pop();
+//            min_heap.push(input[i]);
+//        }
+//    }
+//    while(!min_heap.empty()){
+//        result.push_back(min_heap.top());
+//        min_heap.pop();
+//    }
+//
+//    return result;
+//}
+
+
+//int test(vector<vector<char>>& maze) {
+//    if(maze.empty())
+//        return 0;
+//    int row = maze.size();
+//    int col = maze[0].size();
+//    int num_of_1 = 0;
+//
+//    for (int r = 0; r < row; ++r)
+//        for (int c = 0; c < row; ++c)
+//        {
+//            if (maze[r][c] == '1')
+//                ++ num_of_1;
+//
+//                maze[r][c] = '0';
+//
+//                queue<pair<int, int>> my_queue;
+//
+//                my_queue.push({r, c});
+//
+//                while (!my_queue.empty()) {
+//
+//                    auto rc = my_queue.front();
+//                    my_queue.pop();
+//                    int row = rc.first, col = rc.second;
+//                    if (row - 1 >= 0 && maze[row - 1][col] == '1') {
+//                        my_queue.push({row - 1, col});
+//                        maze[row - 1][col] = '0';
+//                    }
+//                    if (row + 1 < row && maze[row + 1][col] == '1') {
+//                        my_queue.push({row + 1, col});
+//                        maze[row + 1][col] = '0';
+//                    }
+//                    if (col - 1 >= 0 && maze[row][col - 1] == '1') {
+//                        my_queue.push({row, col - 1});
+//                        maze[row][col - 1] = '0';
+//                    }
+//                    if (col + 1 < col && maze[row][col + 1] == '1') {
+//                        my_queue.push({row, col + 1});
+//                        maze[row][col + 1] = '0';
+//                    }
+//                }
+//
+//            }
+//        }
+//}
+//
+
+//int lengthOfLIS(vector<int>& nums) {
+//
+//    if(nums.empty())
+//        return 0;
+//
+//    auto length = nums.size();
+//    int res = 0;
+//    vector<vector<int>> dp(length+1, vector<int>(length+1,10000000));
+//    dp[0][1] = nums[0];
+//    for(int i = 1; i < length; i ++)
+//        for(int j = 2; j <= i+1; j ++)
+//        {
+//            if(dp[i-1][j-1] < nums[i])
+//            {
+//                dp[i][j] = nums[i];
+//                res = max(res, j);
+//            }
+//            else
+//                dp[i][j-1] = nums[i];
+//        }
+//    return res;
+//}
+
+//
+//int min_sub(int num1, int num2)
+//{
+//    int x, y;
+//    x = num1;
+//    y = num2;
+//    while(num1 != num2) {           //循环条件为两数不相等
+//        if(num1 > num2)             //如果第一个数大于第二个数
+//            num1 = num1 - num2;     //两数相减
+//        else
+//            num2 = num2 - num1;
+//    }
+//
+//    return x*y/num1;
+//}
 
 int main()
 {
 
 
-    test_construt_order();
+
+
+
+
+
+
+    return 0;
+
+
+//    test_construt_order();
     // 要记住，默认都是less
 //    vector<int> test = {2,4,8,3,7,5,6};
 //    sort(test.begin(), test.end(), greater<>());
--- test/code.cpp
+++ test/code.cpp
@@ -19,7 +19,7 @@ void backtrack(vector<int> &nums,vector<vector<int>> &res,int i){
     }
 }
 
-// 字典序的全排列 与上面那个相比 是有顺序的
+// 字典序的全排列 与上面那个相比 是有顺序的 这个其实就是不停地进行下一个排序(类似于leetcode上的)
 void backtrack_order(vector<int> &nums,vector<vector<int>> &res,int i){
 
     auto length = nums.size();
@@ -34,7 +34,7 @@ void backtrack_order(vector<int> &nums,vector<vector<int>> &res,int i){
         if (j < 0)
             break;
 
-        for (k = length - 1; k > j&&nums[k] < nums[j]; k--);
+        for (k = length - 1; k > j && nums[k] < nums[j]; k--);
 
         swap(nums[k], nums[j]);
 
@@ -48,6 +48,166 @@ void backtrack_order(vector<int> &nums,vector<vector<int>> &res,int i){
 
 
 
+// 华为笔试第三题 逻辑计算 和 加减乘除类似
+// 遇到& 有三种情况 要考虑 & 的优先级
+// 第一种遇到正常字符或者（，那么就把这个（）内取值，或者正常数字作为第二个数，
+// 接着循环，如果遇到&或者|或者直接到末尾了表示&该计算了，就把第一个数和第二个数做个&操作，作为外层的第一个数
+
+int dfs_normal(string &s, int start_index, int end_index);
+int dfs_kh(int &index, string &s) {
+    for (int i = index; i< s.size(); ++i) {
+        if (s[i] == '(')
+            dfs_kh(++i, s);
+        else if (s[i] == ')') {
+            bool res = dfs_normal(s, index, i);
+            index = i;
+            return res;
+        }
+    }
+}
+int dfs_normal(string &s, int start_index, int end_index) {
+    int first;
+    int second=-1;
+    bool fei = false;
+    for (int i = start_index; i < end_index; ++i) {
+        if (s[i] == '(') {
+            first = dfs_kh(++i, s);
+        }
+        else if (s[i] == '&') {
+            bool fei2 = false;
+            int second2;
+            if (fei) {
+                first = first ^ 1;
+                fei = false;
+            }
+            int j;
+            for (j = i + 1; j < end_index;++j) {  // 因为&的优先级高 所以内循环
+                if (s[j] == '(') {
+                    second2 = dfs_kh(++j, s);
+                }
+                else if (s[j] == '|'||s[j]=='&') {
+                    if (fei2)
+                        second2 = second2 ^ 1;
+                    first = first & second2;
+                    i = j - 1;
+                    break;
+                }
+                else if (s[j] == '!')
+                    fei2 = true;
+                else
+                    second2 = s[j];
+            }
+            if (j == end_index) {
+                first = first&second2;
+                i = j - 1;
+            }
+        }
+        else if (s[i] == '|') {
+            second = dfs_normal(s, i + 1, end_index);
+            break;
+        }
+        else if (s[i] == '!') {
+            fei = true;
+        }
+        else {
+            first = s[i] - '0';
+        }
+    }
+    if (fei) {
+        first = first ^ 1;
+    }
+    if (second == -1)
+        return first;
+    return first|second;
+}
+// 这样调用
+//string s;
+//cin >> s;
+//cout << dfs_normal(s, 0, s.size());
+
+
+
+// 华为第一道 简单的差集 不过要记住字符串的操作方式
+//    vector<int> all_chars(52,0);
+//    vector<int> have_chars(52,0);
+//    vector<int> order;
+//
+//    string s;
+//    getline(cin, s);
+//    bool flag = true;
+//    char temp_char = 0;
+//
+//    for(int i = 0; i < s.size(); i ++)
+//    {
+//        auto item = s[i];
+//        if(flag)
+//        {
+//            if(isalpha(item))     // 判断该字符是不是字母
+//                temp_char = item;
+//
+//            if(isdigit(item))   // 判断该字符是不是数字
+//            {
+//                if(isupper(temp_char))
+//                {
+//                    all_chars[temp_char-'A' + 26] = item - '0';
+//                    order.push_back(temp_char-'A' + 26);
+//                }
+//                else
+//                {
+//                    all_chars[temp_char-'a'] = item - '0';
+//                    order.push_back(temp_char-'a');
+//                }
+//            }
+//            if(item == '@')
+//                flag = false;
+//
+//        } else
+//        {
+//            if(isalpha(item))
+//                temp_char = item;
+//
+//            if(isdigit(item))
+//            {
+//                if(isupper(temp_char))
+//                    have_chars[temp_char-'A' + 26] = item - '0';
+//                else
+//                    have_chars[temp_char-'a'] = item - '0';
+//            }
+//        }
+//    }
+//
+//    for(int i = 0; i < 52; i ++)
+//    {
+//        all_chars[i] = all_chars[i] - have_chars[i];
+//    }
+//
+//
+//    string out = "";
+//    string temp = ":";
+//
+//    for(int i = 0; i < order.size(); i ++)
+//    {
+//        int count = order[i];
+//        if(count < 26 && all_chars[count] != 0)
+//        {
+//            out += char('a' + count) + temp + to_string(all_chars[count]);
+//            out += char(',');
+//
+//        }
+//        if(count >= 26 && all_chars[count] != 0)
+//        {
+//
+//            out += char('A' + count-26) + temp + to_string(all_chars[count]);
+//            out += char(',');
+//
+//        }
+//    }
+//
+//    out.erase(out.end()-1);
+//    cout << out;
+
+
+
 // 不定长数组输入 回车结束
 //while(cin>>num)
 //{
@@ -56,10 +216,7 @@ void backtrack_order(vector<int> &nums,vector<vector<int>> &res,int i){
 //        break;
 //}
 
-
-
-
-//    拼多多笔试最后一题
+//    拼多多笔试最后一题 搭积木的问题 动态规划
 //    int res = 0;
 //    int N;
 //    cin >> N;
@@ -85,19 +242,18 @@ void backtrack_order(vector<int> &nums,vector<vector<int>> &res,int i){
 //    // 这个从上开始往下放
 //    for (int i = 1; i != N; ++i)
 //    {
-//        for (int j = 0; j <= i; ++j) {
+//        for (int j = 0; j <= i; ++j) {    // j高度从0 开始 所以最后结果要+1
 //            if (dp[i - 1][j] <= cube[i].second * 7) {
 //                dp[i][j + 1] = min(dp[i][j + 1], dp[i - 1][j] + cube[i].second);
 //                res = max(res, j + 1);
 //            }
 //            else {
-//                dp[i][j] = min(dp[i][j], dp[i - 1][j]);
+//                dp[i][j] = min(dp[i][j], dp[i - 1][j]);  // 如果放不下 是不是有把当前拿下来的可能性更好
 //            }
 //        }
 //    }
 //
 //    cout << res + 1;
-
 
 
 // 拼多多第二题 dfs题
@@ -136,3 +292,119 @@ void backtrack_order(vector<int> &nums,vector<vector<int>> &res,int i){
 //    else
 //        cout << "false";
 //    return 0;
+
+
+// 网易互娱第一题 算1的个数
+//typedef unsigned long long ull;
+//
+//int batch;
+//cin >> batch;
+//while(batch --)
+//{
+//int n;
+//vector<ull> test_list;
+//int temp;
+//cin >> n;
+//while(n--)
+//{
+//cin>>temp;
+//test_list.push_back(temp);
+//}
+//
+//unordered_map<int, int> num2count;
+//auto len = test_list.size();
+//for(int i = 0; i < len; i ++)
+//{
+//ull num = test_list[i];
+//ull tool = 0x1;
+//int count = 0;
+//
+//while(tool)
+//{
+//if(num & tool)
+//count ++;
+//tool = tool << 1;
+//}
+//
+//if(!num2count.count(count))
+//num2count.insert({count, 1});
+//num2count[count] ++;
+//
+//}
+//
+//auto res = num2count.size();
+//cout << res;
+//
+//}
+
+
+// 网易互娱第二题 游泳池问题 做题思想学一学
+//int batch;
+//cin >> batch;
+//while(batch --)
+//{
+//int m;
+//cin >> m;
+//int t;
+//cin >> t;
+//int m1;
+//cin >> m1;
+//int t1;
+//cin >> t1;
+//int m2;
+//cin >> m2;
+//int t2;
+//cin >> t2;
+//
+//int curr_m = 0;
+//
+//for(int i = 0; i < t; i ++)
+//{
+//if(i % (2*t1) < t1 && i % (2*t2) < t2)
+//curr_m = min(m, max(0, curr_m + m1 - m2));
+//if(i % (2*t1) < t1 and i % (2*t2) >= t2)
+//curr_m = min(m, curr_m + m1);
+//if(i % (2*t1) >= t1 and i % (2*t2) < t2)
+//curr_m = max(0, curr_m - m2);
+//}
+//
+//cout << curr_m << endl;
+//
+//}
+
+
+// 网易互娱第三题 不是很难 双指针即可
+//int T;
+//cin >> T;
+//while (T > 0) {
+//T--;
+//string s;
+//cin >> s;
+//int res=0;
+//int start = 0;
+//int end = 0;
+//int count = 0;
+//
+//if (s.empty()) {
+//cout << res << endl;
+//break;
+//}
+//while (end < s.size()) {
+//if (s[end] != 'N') {
+//count++;
+//}
+//end++;
+//if (count >= 3) {
+//while (start < end) {
+//if (s[start] != 'N') {
+//count--;
+//}
+//start++;
+//if (count <= 2)
+//break;
+//}
+//}
+//res = max(res, end - start);
+//}
+//cout << res << endl;
+//}
